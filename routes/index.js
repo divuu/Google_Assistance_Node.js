120deae4607be7a6514de06442be8b46d3d8ce6b
unknown <developer@technomediasoft.com>
2018-12-04 17:26:23 +0530

    format

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+let resData = {
+  speech: "",
+  displayText: "Testing",
+  data:{
+    google: {
+      expected_user_response: false,
+      is_ssml: false,
+      permission_request:{
+        opt_context: '',
+        permissions: []
+      }
+    }
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/webhook', function(req, res, next){
-  let data  = {
-    'testing': true
-  }
   console.log(req.body);
   console.log("User", req.body.originalDetectIntentRequest.payload.user);
-  res.json(data)
+  resData.speech = 'Your bus is 5 mins away';
+  resData.displayText = 'Your bus is 5 mins away'
+  res.json(resData);
 });
 
 module.exports = router;
