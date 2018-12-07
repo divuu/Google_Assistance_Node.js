d06977d1a9933a3f624dff1944008927a97ee1a3
Kumar Yashasvi <developer@technomediasoft.com>
2018-12-07 11:38:54 +0530

    code refactor

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,26 @@ let basicResponse = {
   }
 }
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/webhook', function(req, res, next){
-  if(req.body.queryResult.action === "action_register"){
-    basicResponse.fulfillmentText = `Welcome to Route Alert! Good to have you with us. I have registered your number ${req.body.queryResult.parameters.phone}. Thank you.`
-    res.json(basicResponse);
-  }else{
+router.post('/webhook', function (req, res, next) {
+  if (req.body.queryResult.action === "action_register") {
+    //create a deep copy
+    thisResponse = JSON.parse(JSON.stringify(basicResponse));
+    
+    res.json(register(thisResponse, req.body));
+  } else {
     res.json(basicResponse);
   }
 });
 
+// handles registration process
+// adds in the data base
+function register(bResponse, requestObj) {
+  //logic to add to db
+  bResponse.fulfillmentText = `Welcome to Route Alert! Good to have you with us. I have registered your number ${requestObj.queryResult.parameters.phone}. Thank you.`
+  return bResponse;
+}
 module.exports = router;
