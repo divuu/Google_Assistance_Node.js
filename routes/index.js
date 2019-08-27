640f4f02bac2ad8824aa3e379cbd33e41c9d40c2
divuu <utkarshsri0701@gmail.com>
2019-08-27 18:42:18 +0530

    US

--- routes/index.js
+++ routes/index.js
@@ -62,14 +62,14 @@ let simpleResponse = {
               textToSpeech: "Today is not a holiday."
             }
           },
-          {
-            basicCard: {
-              title: "Bus Details",
-              subtitle:
-                "MPS Transport Manager :- Avinash Tiwari \nMobile Number :- 5678643522 \nBus Registration Number :- AS 64 GF3426",
-              formattedText: "Via swamy Vivekananda road"
-            }
-          },
+          // {
+          //   basicCard: {
+          //     title: "Bus Details",
+          //     subtitle:
+          //       "MPS Transport Manager :- Avinash Tiwari \nMobile Number :- 5678643522 \nBus Registration Number :- AS 64 GF3426",
+          //     formattedText: "Via swamy Vivekananda road"
+          //   }
+          // },
           {
             carouselBrowse: {
               items: [
