939135dd7384b30d14a861c64ccd39fc513270d1
divuu <utkarshsri0701@gmail.com>
2019-08-27 17:15:43 +0530

    US

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,7 @@ let simpleResponse = {
                   description: "View the live location in Google Map",
                   image: {
                     url:
-                      "https://res.cloudinary.com/techmky/image/upload/v1566890723/google_map2_mnl6ns.png",
+                      "https://res.cloudinary.com/techmky/image/upload/v1566890722/call_e6w5rk.png",
                     accessibilityText: "Image alternate text"
                   }
                 },
@@ -91,19 +91,19 @@ let simpleResponse = {
                   description: "Share Bus Live location on WhatsApp",
                   image: {
                     url:
-                      "https://res.cloudinary.com/techmky/image/upload/v1566899238/whatsapp_t9tj13.png",
+                      "https://res.cloudinary.com/techmky/image/upload/v1566890722/call3_wmr7qk.png",
                     accessibilityText: "Image alternate text"
                   }
                 },
                 {
                   title: "Call RouteAlert",
                   openUrlAction: {
-                    url: "tel:09066841400"
+                    url: "https://example.com"
                   },
                   description: "Call RouteAlert Support Team For Any Help !",
                   image: {
                     url:
-                      "https://res.cloudinary.com/techmky/image/upload/v1566906021/call_resized_isstvt.png",
+                      "https://res.cloudinary.com/techmky/image/upload/v1566890722/call3_wmr7qk.png",
                     accessibilityText: "Image alternate text"
                   }
                 }
