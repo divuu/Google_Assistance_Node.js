6bdb7472c9607e5aaa4306e62ffb092ad55deac3
divuu <utkarshsri0701@gmail.com>
2019-08-29 17:18:50 +0530

    US

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,15 @@ var jwtDecode = require("jwt-decode");
 let router = express.Router();
 const jwt = require("jsonwebtoken");
 const keys = require("../assets/keys.json");
-let resp = [];
 let sysuserdataArray = [];
-//let request = [];
 let test = [];
 let responseNumber = 0;
-//let localvariable = "Hi i am new variable in the node.js";
+var sessionUserData = {};
 
 let basicResponse = {
   payload: {
     google: {
-      expectUserResponse: true,
+      expectUserResponse: false,
       richResponse: {
         items: [
           {
@@ -157,6 +155,7 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
   //req.render('index',{title:'Express'});
 });
+
 // router.post("/webhook", function(req, res, next) {
 //   console.log("REQUEST BODY", JSON.stringify(req.body));
 
@@ -212,6 +211,7 @@ router.post("/webhook", function(req, res, next) {
   if (req.body.queryResult.action == "action_welcome") {
     var PIN = req.body.queryResult.parameters.PIN;
     //let spquery = "CALL sp_sysuser_verification(" + PIN + ")";
+    //Validation of pin()
     let spquery = "CALL sp_rga_pin_verification('" + PIN + "')";
     console.log("spquery", spquery);
     db.query(spquery, true, (error, results, fields) => {
@@ -222,99 +222,124 @@ router.post("/webhook", function(req, res, next) {
       console.log("Result", results);
       console.log("Result[0]", results[0]);
 
-      var tabledata = JSON.stringify(results[0]);
-      var tabledata_json = JSON.parse(tabledata);
-
-      //console.log(fields);
-      sysuserdataArray.push({
-        response_number: responseNumber++,
-        length: tabledata_json.length,
-        payload: tabledata_json
-      });
-
-      console.log(tabledata);
-      console.log(tabledata_json);
       //console.log("Actual Name of sysuser", tabledata_json[0].name);
-      if (tabledata_json.length == 0) {
-        basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `! It Seems your PIN is Incorrect Or You are Not Authorised For This Service. Kindly Contact RouteAlert Support Team.`;
+
+      if (results[0].length == 0) {
+        basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = ` Sorry, your PIN is invalid. We cant find the user in our records. Thank you for your interest in RouteAlert. `;
         res.json(basicResponse);
       } else {
+        var tabledata = JSON.stringify(results[0]);
+        var tabledata_json = JSON.parse(tabledata);
+
+        sysuserdataArray.push({
+          response_number: responseNumber++,
+          length: tabledata_json.length,
+          payload: tabledata_json
+        });
+
+        console.log("user data Array", sysuserdataArray);
+        console.log("tabledat_json", tabledata_json);
+
         // Check data values for sysuser or Parent
-        if (tabledata[0].passenger_id) {
-          console.log("PIN from Parent master");
+        if (tabledata_json[0].passenger_id) {
+          console.log("PIN Verfication Passenger master");
+
+          basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `  Welcome ${tabledata_json[0].name} of ${tabledata_json[0].org_name}. Do you want to know the location of your school bus ? `;
+          //Store User Data With Session
+
+          tabledata_json
+            .filter(val => val.running_route_id == 0)
+            .forEach(val => {
+              userDataWithSession(
+                req.body.originalDetectIntentRequest.payload.conversation
+                  .conversationId,
+                val
+              );
+            });
+
+          //res.json(sessionUserData);
+          res.json(basicResponse);
         } else {
-          console.log("PIN FROM Sys User");
+          console.log("PIN Verfication SysUser Master");
 
           //Check for sysUser Organization
-          if (sysuserdataArray.length > 1) {
-            basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Hi ${
-              tabledata_json[0].name
-            }! Welcome to Route Alert. Which school do you want to check in to?`;
+          if (tabledata_json.length > 1) {
+            console.log("In multiple School");
+            basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Hi ${tabledata_json[0].role_name}  ${tabledata_json[0].user_name}. Welcome to Route Alert. Which School's information do you want to know about ?`;
+            //Store User Data With Session
+            userDataWithSession(
+              req.body.originalDetectIntentRequest.payload.conversation
+                .conversationId,
+              tabledata_json
+            );
             res.json(basicResponse);
           } else {
-            //if (sysuserdataArray.length == 1) {
-            basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Hi ${
-              tabledata_json[0].name
-            }! Welcome to Route Alert. Which School Bus location Do you want to know ?`;
-            res.json(basicResponse);
+            if (tabledata_json.length == 1) {
+              console.log("In Single School");
+              basicResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Hi ${tabledata_json[0].role_name}  ${tabledata_json[0].user_name}. Welcome to Route Alert. Which School Bus' location do you want to know about ?`;
+              //Store User Data With Session
+              tabledata_json.forEach(val => {
+                userDataWithSession(
+                  req.body.originalDetectIntentRequest.payload.conversation
+                    .conversationId,
+                  val
+                );
+              });
+              res.json(basicResponse);
+            }
           }
         }
       }
-      dumpIntoDatabase(
-        req.body.queryResult.action,
-        JSON.stringify(req.body),
-        JSON.stringify(res),
-        "Test"
-      );
     });
   }
 
   // For SysUser School Bus Info
-  // Send Parameters :- take Bus Number.
+  // Send Parameters :- input Bus Number eg:- MPS Route 1.
   // Receive Parameters :- give back the Bus Details.
   if (req.body.queryResult.action == "Action_Sysuser_schoolbusDetail") {
-    console.log("REQUEST BODY", JSON.stringify(req.body));
-    // var busRouteID = req.body.queryResult.parameters.BusRouteID;
-    // console.log("BusRouteID", busRouteID);
-    // let prefix = ["MPS Route"];
-    // let finalStr;
-    // if (typeof busRouteID == "number") {
-    //   finalStr = prefix + " " + busRouteID;
-    //   console.log("Bus String", finalStr);
-    // } else {
-    //   let strafter = busRouteID.replace(/[A-Za-z$-]/g, "");
-    //   finalStr = prefix + " " + strafter;
-    //   console.log("Bus String", finalStr);
-    // }
-    // let spquery = "CALL sp_assistant_stop_for_sysuser('" + finalStr + "')";
-    // console.log("spquery", spquery);
-    // db.query(spquery, true, (error, results, fields) => {
-    //   if (error) {
-    //     return console.log(error.message);
-    //   }
-    //   console.log("Result", results);
-    //   console.log("Result[0]", results[0]);
-    //   var tabledata = JSON.stringify(results[0]);
-    //   var tabledata_json = JSON.parse(tabledata);
-    //   console.log(tabledata);
-    //   console.log(tabledata_json);
-    //   console.log("Actual address", tabledata_json[0].stop_name);
-    //   simpleResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Ok ! I found your Bus. Your Bus MPS Route 1 was Last seen 2 Min Ago near
-    //   ${
-    //     tabledata_json[0].stop_name
-    //   }. Please Click the Link below to view in map.`;
-    //   // simpleResponse.payload.google.richResponse.items[1].basicCard.formattedText = ` ${
-    //   //   tabledata_json[0].stop_name
-    //   // }`;
-    //   // simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[0].openUrlAction.url = `http://maps.google.com/maps?daddr=${
-    //   //   tabledata_json[0].location
-    //   // }&amp;ll=`;
-    //   // simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[1].openUrlAction.url = `https://api.whatsapp.com/send?text=${
-    //   //   tabledata_json[0].location
-    //   // }`;
-    //   res.json(simpleResponse);
-    //   //res.json(basicResponse);
-    // });
+    var busRouteNumber = req.body.queryResult.parameters.BusRouteNumber; // Route 1 or Bus 1 or MPS Route 1... it will return 1 ( fetches the number )
+    console.log("BusRouteNumber", busRouteNumber);
+    let prefix =
+      sessionUserData[
+        req.body.originalDetectIntentRequest.payload.conversation.conversationId
+      ].short_name;
+    let finalStr;
+    if (typeof busRouteNumber == "number") {
+      finalStr = prefix + " " + busRouteNumber;
+      console.log("Bus String", finalStr);
+    } else {
+      let strafter = busRouteNumber.replace(/[A-Za-z$-]/g, "");
+      finalStr = prefix + " " + strafter;
+      console.log("Bus String", finalStr);
+    }
+    let spquery = "CALL sp_assistant_stop_for_sysuser('" + finalStr + "')";
+    console.log("spquery", spquery);
+    db.query(spquery, true, (error, results, fields) => {
+      if (error) {
+        return console.log(error.message);
+      }
+      console.log("Result", results);
+      console.log("Result[0]", results[0]);
+      var tabledata = JSON.stringify(results[0]);
+      var tabledata_json = JSON.parse(tabledata);
+      console.log(tabledata);
+      console.log(tabledata_json);
+      console.log("Actual address", tabledata_json[0].stop_name);
+
+      simpleResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Ok ! I found your Bus. Your Bus MPS Route 1 was Last seen 2 Min Ago near
+      ${tabledata_json[0].stop_name}. Please Click the Link below to view in map.`;
+      // simpleResponse.payload.google.richResponse.items[1].basicCard.formattedText = ` ${
+      //   tabledata_json[0].stop_name
+      // }`;
+      // simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[0].openUrlAction.url = `http://maps.google.com/maps?daddr=${
+      //   tabledata_json[0].location
+      // }&amp;ll=`;
+      // simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[1].openUrlAction.url = `https://api.whatsapp.com/send?text=${
+      //   tabledata_json[0].location
+      // }`;
+      res.json(simpleResponse);
+      //res.json(basicResponse);
+    });
   }
 
   //Temporary Disabled enable for parent
@@ -396,15 +421,9 @@ router.post("/webhook", function(req, res, next) {
       console.log("Actual Address", json[0].address);
       simpleResponse.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Ok ! I found your Bus. Your Bus MPS Route 1 was Last seen 2 Min Ago near
         ${json[0].address}. Please Click the Link below to view in map.`;
-      simpleResponse.payload.google.richResponse.items[1].basicCard.formattedText = ` ${
-        json[0].address
-      } `;
-      simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[0].openUrlAction.url = `http://maps.google.com/maps?daddr=${
-        json[0].location
-      }&amp;ll=`;
-      simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[1].openUrlAction.url = `https://api.whatsapp.com/send?text=${
-        json[0].location
-      }`;
+      simpleResponse.payload.google.richResponse.items[1].basicCard.formattedText = ` ${json[0].address} `;
+      simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[0].openUrlAction.url = `http://maps.google.com/maps?daddr=${json[0].location}&amp;ll=`;
+      simpleResponse.payload.google.richResponse.items[1].basicCard.buttons[1].openUrlAction.url = `https://api.whatsapp.com/send?text=${json[0].location}`;
 
       res.json(simpleResponse);
     });
@@ -454,9 +473,7 @@ function register(bResponse, requestObj, decoded) {
       //   "! Good to have you with us. I have registered your number <say-as interpret-as='telephone'>" +
       //   +"<say-as> with your email-Id, " +
       //   +". Thank you!</speak>"
-      ssml: `<speak>Hi! Good to have you with us. I have registered your number <say-as interpret-as=\"characters\">${
-        requestObj.queryResult.parameters.MobileNo
-      }</say-as> with your email-Id, ${decoded.email}. Thank you!</speak>`
+      ssml: `<speak>Hi! Good to have you with us. I have registered your number <say-as interpret-as=\"characters\">${requestObj.queryResult.parameters.MobileNo}</say-as> with your email-Id, ${decoded.email}. Thank you!</speak>`
       //ssml: `<speak>Hi! Good to have you with us.`
     }
   });
@@ -475,28 +492,15 @@ function verifyJWT(token, cert) {
   });
 }
 
-function dumpIntoDatabase(intentName, request, resposnse, final) {
-  let logquery =
-    "CALL sp_google_log(" +
-    intentName +
-    "," +
-    request +
-    "," +
-    resposnse +
-    "," +
-    final +
-    ")";
-  console.log(logquery);
-  db.query(logquery, true, error => {
-    if (error) {
-      return console.log(error.message);
-    } else console.log("------------------------->>>>>> DB Write successful");
-  });
+//Saves the User data after pin verification with ConversationID
+function userDataWithSession(conversationID, parameter) {
+  if (Array.isArray(parameter)) {
+    sessionUserData[conversationID] = {
+      payload: Array.from(parameter)
+    };
+  } else {
+    sessionUserData[conversationID] = Object.assign({}, parameter);
+  }
 }
 
 module.exports = router;
-
-// , {
-//   audience: keys.CLIENT_ID,
-//   issuer: "https://accounts.google.com"
-// }
