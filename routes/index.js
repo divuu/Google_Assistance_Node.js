1c7d495f809d09bc40632802d9e175d833e4032d
divuu <utkarshsri0701@gmail.com>
2019-09-06 20:31:41 +0530

    US

--- routes/index.js
+++ routes/index.js
@@ -87,7 +87,7 @@ let simpleResponse = {
                 {
                   title: "Call RouteAlert",
                   openUrlAction: {
-                    url: "tel:9066841400"
+                    url: "https://www.google.com"
                   },
                   description: "Call RouteAlert Support Team For Any Help !",
                   image: {
